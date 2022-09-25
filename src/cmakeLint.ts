cca3443c203499649cd21336fd30e4ab34839018
brobeson <brobeson@users.noreply.github.com>
2022-09-24 19:53:05 -0600

    I combined typescript files
    I moved the functionality from extension.ts into cmakeLint.ts and made
    cmakeLint.ts the main execution point for the extension in package.json

--- src/cmakeLint.ts
+++ src/cmakeLint.ts
@@ -1,7 +1,51 @@
 import { spawn } from "child_process";
 import * as vscode from "vscode";
 
-export async function lintDocument(file: string) {
+export function activate(context: vscode.ExtensionContext) {
+  let subscriptions = context.subscriptions;
+  let diagnostics = vscode.languages.createDiagnosticCollection("cmakeLint");
+  subscriptions.push(diagnostics);
+
+  async function cmakeLintAllFiles() {
+    let allFiles = await vscode.workspace.findFiles("**/CMakeLists.txt");
+    allFiles = allFiles.concat(await vscode.workspace.findFiles("**/*.cmake"));
+    for (const cmakeFile of allFiles) {
+      diagnostics.set(cmakeFile, await lintDocument(cmakeFile.fsPath));
+    }
+  }
+
+  async function cmakeLintActiveDocument() {
+    if (
+      vscode.window.activeTextEditor !== undefined &&
+      vscode.window.activeTextEditor.document.languageId === "cmake"
+    ) {
+      diagnostics.set(
+        vscode.window.activeTextEditor.document.uri,
+        await lintDocument(vscode.window.activeTextEditor.document.uri.fsPath)
+      );
+    }
+  }
+
+  context.subscriptions.push(
+    vscode.commands.registerCommand("cmakeLint.scanAllFiles", cmakeLintAllFiles)
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "cmakeLint.scanActiveFile",
+      cmakeLintActiveDocument
+    )
+  );
+  context.subscriptions.push(
+    vscode.workspace.onDidOpenTextDocument(cmakeLintActiveDocument)
+  );
+  context.subscriptions.push(
+    vscode.workspace.onDidSaveTextDocument(cmakeLintActiveDocument)
+  );
+}
+
+export function deactivate() {}
+
+async function lintDocument(file: string) {
   const output = await runCmakeLint(file);
   return createDiagnosticsFromOutput(output);
 }
